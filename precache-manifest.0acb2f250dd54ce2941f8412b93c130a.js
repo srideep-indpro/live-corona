05e6db4f3c255a8ffd1b89147eeff9a47b07d4fe
SB067746 <SB067746@cerner.net>
2020-03-25 13:43:19 +0530

    Updates

---
+++ precache-manifest.0acb2f250dd54ce2941f8412b93c130a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a5e4125f2c6800ee7f87e7ca4da711",
+    "url": "/live-corona/index.html"
+  },
+  {
+    "revision": "0fe146fcf2a69c038453",
+    "url": "/live-corona/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e02d738ee920d53ebb94",
+    "url": "/live-corona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0fe146fcf2a69c038453",
+    "url": "/live-corona/static/js/2.d5e98258.chunk.js"
+  },
+  {
+    "revision": "1035eb16e0f287e503e9fbe9e7321198",
+    "url": "/live-corona/static/js/2.d5e98258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e02d738ee920d53ebb94",
+    "url": "/live-corona/static/js/main.d97e46b5.chunk.js"
+  },
+  {
+    "revision": "4824118f553f1d05a55b",
+    "url": "/live-corona/static/js/runtime-main.73c22dd6.js"
+  }
+]);
